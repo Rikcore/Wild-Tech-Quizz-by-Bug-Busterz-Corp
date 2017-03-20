3b1f4074acbeba2e865e19cd1ae1ea873e930807
Zilio Bruno <bruno.zilio31@gmail.com>
2017-03-20 14:12:56 +0100

    test commit, sur branche

---
+++ app/src/main/java/bugbusterzcorp/wildtechquizz/MyQuizzActivity.java
@@ -0,0 +1,13 @@
+package bugbusterzcorp.wildtechquizz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyQuizzActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_quizz);
+    }
+}

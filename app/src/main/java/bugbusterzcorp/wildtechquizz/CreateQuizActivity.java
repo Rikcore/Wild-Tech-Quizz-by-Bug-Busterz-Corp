ebf62fb57ec7164c80db395533640b0994dcced7
Kevin Anin <kbacreation@gmail.com>
2017-03-31 14:01:40 +0200

    added quiz creation feature

--- app/src/main/java/bugbusterzcorp/wildtechquizz/CreateQuizActivity.java
+++ app/src/main/java/bugbusterzcorp/wildtechquizz/CreateQuizActivity.java
@@ -5,25 +5,36 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+
 
 public class CreateQuizActivity extends AppCompatActivity {
+
+
 
 
 
     String mCorrectAnswer;
     RadioButton radioButtonFirstChoice;
     RadioButton radioButtonSecondChoice;
-    FloatingActionButton floatingActionButtonAddQuestion;
+    //FloatingActionButton floatingActionButtonAddQuestion;
     String mQuestion;
     String mChoiceA;
     String mChoiceB;
-
+    Button validate;
+    ArrayList questionList;
 
 
     @Override
@@ -38,30 +49,83 @@ public class CreateQuizActivity extends AppCompatActivity {
         RadioButton radioButtonFirstChoice = (RadioButton) findViewById(R.id.radioButtonFirstChoice);
         RadioButton radioButtonSecondChoice = (RadioButton) findViewById(R.id.radioButtonSecondChoice);
         FloatingActionButton floatingActionButtonAddQuestion = (FloatingActionButton) findViewById(R.id.floatingActionButtonAddQuestion);
-
+        Button validate =  (Button) findViewById(R.id.validate);
+        final ArrayList<QuestionClass> questionList = new ArrayList<>();
 
         mQuestion = editTextQuestion.getText().toString();
         mChoiceA = editTextFirstChoice.getText().toString();
         mChoiceB = editTextSecondChoice.getText().toString();
 
 
-        if(radioButtonFirstChoice.isChecked()){
-            mCorrectAnswer = mChoiceA ;
-            //radioButtonSecondChoice.setChecked(false);
+     validate.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
 
-        }
-
-        else if(radioButtonSecondChoice.isChecked()){
-            mCorrectAnswer = mChoiceB ;
-            //radioButtonFirstChoice.setChecked(false);
-        }
+             QuizClass newQuiz = new QuizClass(questionList);
+             FirebaseDatabase database = FirebaseDatabase.getInstance();
+             DatabaseReference ref = database.getReference("Quizz");
+             ref.push().setValue(newQuiz);
 
 
+         }
+     });
 
+      floatingActionButtonAddQuestion.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              /*Toast toastSuccess = Toast.makeText(context, textSuccess, durationSuccess);
+
+              CharSequence textFail = "Veuillez remplir tous les champs et cocher la bonne réponse";
+              int durationFail = Toast.LENGTH_SHORT;
+
+
+              Toast toastFail = Toast.makeText(context, textFail, durationFail); */
+
+              EditText editTextQuestion = (EditText) findViewById(R.id.editTextQuestion);
+              EditText editTextFirstChoice = (EditText) findViewById(R.id.editTextFirstChoice);
+              EditText editTextSecondChoice = (EditText) findViewById(R.id.editTextSecondChoice);
+              RadioButton radioButtonFirstChoice = (RadioButton) findViewById(R.id.radioButtonFirstChoice);
+              RadioButton radioButtonSecondChoice = (RadioButton) findViewById(R.id.radioButtonSecondChoice);
+
+              if(radioButtonFirstChoice.isChecked()){
+                  mCorrectAnswer = mChoiceA;
+                  //radioButtonSecondChoice.setChecked(false);
+
+              }
+
+              else if(radioButtonSecondChoice.isChecked()){
+                  mCorrectAnswer = mChoiceB;
+                  //radioButtonFirstChoice.setChecked(false);
+              }
+
+              if(editTextQuestion.length() > 0 && editTextFirstChoice.length() > 0 && editTextSecondChoice.length() > 0 ) {
+
+                  mQuestion = editTextQuestion.getText().toString();
+                  mChoiceA = editTextFirstChoice.getText().toString();
+                  mChoiceB = editTextSecondChoice.getText().toString();
+
+                  QuestionClass newQuestion = new QuestionClass(mQuestion, mChoiceA, mChoiceB, mCorrectAnswer);
+
+                  questionList.add(newQuestion);
+                  //toastSuccess.show();
+                  editTextQuestion.setText("");
+                  editTextFirstChoice.setText("");
+                  editTextSecondChoice.setText("");
+
+             /* } else{
+                  toastFail.show();
+              } */
+          }
+      }
+
+
+
+
+    });
 
     }
 
-    public void addQuestion(View view){
+   /* public void addQuestion(View view){
 
         Context context = getApplicationContext();
         CharSequence textSuccess = "Question enregistrée";
@@ -76,35 +140,43 @@ public class CreateQuizActivity extends AppCompatActivity {
 
         Toast toastFail = Toast.makeText(context, textFail, durationFail);
 
-
-
-
-
         EditText editTextQuestion = (EditText) findViewById(R.id.editTextQuestion);
         EditText editTextFirstChoice = (EditText) findViewById(R.id.editTextFirstChoice);
         EditText editTextSecondChoice = (EditText) findViewById(R.id.editTextSecondChoice);
+        RadioButton radioButtonFirstChoice = (RadioButton) findViewById(R.id.radioButtonFirstChoice);
+        RadioButton radioButtonSecondChoice = (RadioButton) findViewById(R.id.radioButtonSecondChoice);
 
-        mQuestion = editTextQuestion.getText().toString();
-        mChoiceA = editTextFirstChoice.getText().toString();
-        mChoiceB = editTextSecondChoice.getText().toString();
+        if(radioButtonFirstChoice.isChecked()){
+            mCorrectAnswer = mChoiceA;
+            //radioButtonSecondChoice.setChecked(false);
+
+        }
+
+        else if(radioButtonSecondChoice.isChecked()){
+            mCorrectAnswer = mChoiceB;
+            //radioButtonFirstChoice.setChecked(false);
+        }
 
         if(editTextQuestion.length() > 0 && editTextFirstChoice.length() > 0 && editTextSecondChoice.length() > 0 ) {
 
+            mQuestion = editTextQuestion.getText().toString();
+            mChoiceA = editTextFirstChoice.getText().toString();
+            mChoiceB = editTextSecondChoice.getText().toString();
 
+            QuestionClass newQuestion = new QuestionClass(mQuestion, mChoiceA, mChoiceB, mCorrectAnswer);
 
+            questionList.add(newQuestion);
             toastSuccess.show();
             editTextQuestion.setText("");
             editTextFirstChoice.setText("");
             editTextSecondChoice.setText("");
-
-            QuestionClass newQuestion = new QuestionClass(mQuestion, mChoiceA, mChoiceB, mCorrectAnswer);
 
         } else{
             toastFail.show();
         }
 
 
-    }
+    }*/
 
 
 }

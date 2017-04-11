db33f69bc48c9736a0fb2c00897ce0ce090444e0
[Rikcore] <[cyprien.chamayou@gmail.com]>
2017-04-11 17:46:00 +0200

    Commit avant merge 11/04

--- app/src/main/java/bugbusterzcorp/wildtechquizz/MyReceiver.java
+++ app/src/main/java/bugbusterzcorp/wildtechquizz/MyReceiver.java
@@ -8,13 +8,13 @@ import android.content.Intent;
  * Created by apprenti on 10/04/17.
  */
 
+
 public class MyReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(intent.ACTION_BOOT_COMPLETED)){
-            Intent myIntent = new Intent (MyService.INTENT_DISPLAY_NOTIF);
-            myIntent.setClass(context, MyService.class);
-            context.startService(myIntent);
+            context.startService(new Intent(context, MyService.class));
+
         }
     }
 }
--- app/src/main/java/bugbusterzcorp/wildtechquizz/MyService.java
+++ app/src/main/java/bugbusterzcorp/wildtechquizz/MyService.java
@@ -13,11 +13,20 @@ import android.support.annotation.RequiresApi;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MyService extends Service {
 
-    public static final String INTENT_DISPLAY_NOTIF = "DemoService.DisplayNotif";
+    public static final DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Quizz");
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    String creatorName;
+
     public MyService() {
+
     }
 
     @Override
@@ -29,22 +38,45 @@ public class MyService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        if (intent != null){
-            if (intent.getAction().equals(INTENT_DISPLAY_NOTIF)){
+        ref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                creatorName = (String) dataSnapshot.child("username").getValue();
                 displayNotification();
             }
 
-        }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         return Service.START_STICKY;
+
     }
 
     private void displayNotification() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        FirebaseUser user =FirebaseAuth.getInstance().getCurrentUser();
         NotificationManager notifManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         Notification noti = new Notification.Builder(this)
-                .setContentTitle("Un nouveau quizz est disponible !")
-                .setContentText(user.getDisplayName()+" a créé un quizz")
-                .setSmallIcon(R.drawable.edward)
+                .setContentTitle("Wild Tech Quizz")
+                .setContentText(user.getDisplayName()+", un nouveau quizz de "+creatorName+" vous attend!")
+                .setSmallIcon(R.mipmap.interrogation_burned)
                 .setContentIntent(PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0))
                 .build();
 
@@ -52,4 +84,6 @@ public class MyService extends Service {
 
 
     }
+
+
 }
